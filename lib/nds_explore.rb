9cafe1123bc548c5fed4bb1fa6768a51359d94cb
Radniel Padron <radnielpadron@gmail.com>
2020-01-23 18:16:46 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -15,7 +15,7 @@ def print_first_directors_movie_titles
  #binding.pry
  i = 0
  while i < directors_database.length do
-  puts directors_database[i]
+  # puts directors_database[i]
   i += 1
   end
 
